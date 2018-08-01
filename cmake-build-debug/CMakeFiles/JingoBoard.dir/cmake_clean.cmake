1be2ef71ff1be7478f82e687974a1688fea28e55
Bill King <BillCKing@gmail.com>
2018-08-01 15:25:10 -0400

    Further edits

---
+++ cmake-build-debug/CMakeFiles/JingoBoard.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JingoBoard.dir/main.cpp.o"
+  "JingoBoard.pdb"
+  "JingoBoard.exe"
+  "libJingoBoard.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JingoBoard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
