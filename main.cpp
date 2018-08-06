70ac31823197a618f160f34fbbef9aaacfc3bf89
Bill King <BillCKing@gmail.com>
2018-08-06 15:11:28 -0400

    Add keyboard hooks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <Windows.h>
+
+// Callback function for keyboard hook
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
+
+// Keyboard hook
+HHOOK keyboardHook;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+	printf("Starting keyboard hook...");
+	keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, 0, 0);    // Using low level hooks. Need to be mindful of timeouts.
+
+	MSG msg{0}; // MSG structure that receives message information from the thread's message queue.
+	while (GetMessage(&msg, NULL, 0, 0) != 0);  // Main message loop quits when function receives WM_QUIT.
+	UnhookWindowsHookEx(keyboardHook);
+	return 0;
+}
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+
+
 }
