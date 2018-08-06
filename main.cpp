53d660621f488e0b87ebd825630a583f1f9f711b
Bill King <BillCKing@gmail.com>
2018-08-06 16:13:27 -0400

    Add more keyboard hook edits

--- main.cpp
+++ main.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
 #include <Windows.h>
 
-// Callback function for keyboard hook
 LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
 
-// Keyboard hook
 HHOOK keyboardHook;
 
 int main() {
 	puts("Starting keyboard hook...");
 	keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, 0, 0);    // Using low level hooks. Need to be mindful of timeouts.
-	puts("Keyboard hook started.");
-	puts("Preparing message queue...");
-	MSG msg{0}; // MSG structure that receives message information from the thread's message queue.
-	puts("System message queue prepared.");
+	MSG msg{0};
 	puts("Entering system message loop...");
-	while (GetMessage(&msg, NULL, 0, 0) != 0);  // Main message loop quits when function receives WM_QUIT.
-	puts("Exited system message loop.");
-	puts("Terminating keyboard hook.");
+	while (GetMessage(&msg, NULL, 0, 0) != 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	};
 	UnhookWindowsHookEx(keyboardHook);
-	puts("Keyboard hook terminated.");
+	puts("Keyboard hook ended.");
 	return 0;
 }
 
 LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
-	PKBDLLHOOKSTRUCT key = (PKBDLLHOOKSTRUCT) lParam;   // Initializing key code variable.
-	printf("%c", key->vkCode);
-	return CallNextHookEx(keyboardHook, nCode, wParam, lParam); // Restart the hook loop.
+	PKBDLLHOOKSTRUCT key = (PKBDLLHOOKSTRUCT) lParam;
+
+
+	if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
+		puts("");
+		//		Beep(523,10);
+		printf("Key pressed: %x", key->vkCode);
+	} else if (key->vkCode == VK_ESCAPE) {
+
+
+	}
+	return CallNextHookEx(keyboardHook, nCode, wParam, lParam);
 }
